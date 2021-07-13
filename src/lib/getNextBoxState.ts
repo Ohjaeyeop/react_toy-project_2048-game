d6c1b1bb19d55c3559a7cd8762ec132c0e123ac1
Ohjaeyeop <39650591+Ohjaeyeop@users.noreply.github.com>
2021-07-13 14:17:02 +0900

    getNextBoxState 수정

--- src/lib/getNextBoxState.ts
+++ src/lib/getNextBoxState.ts
@@ -6,15 +6,12 @@ function getNextBoxState(
   if (direction === 1) {
     for (let i = 0; i < 4; i++) {
       for (let j = 0; j < 4; j++) {
-        if (boxState[i][j] > 1) {
+        if (boxState[i][j] > 1 && isMovable[i][j]) {
           if (j - 1 >= 0) {
             if (boxState[i][j - 1] === 1) {
               boxState[i][j - 1] = boxState[i][j];
               boxState[i][j] = 1;
-            } else if (
-              boxState[i][j - 1] === boxState[i][j] &&
-              isMovable[i][j]
-            ) {
+            } else if (boxState[i][j - 1] === boxState[i][j]) {
               isMovable[i][j - 1] = false;
               boxState[i][j - 1] += boxState[i][j];
               boxState[i][j] = 1;
@@ -27,15 +24,12 @@ function getNextBoxState(
   if (direction === 2) {
     for (let i = 0; i < 4; i++) {
       for (let j = 3; j >= 0; j--) {
-        if (boxState[i][j] > 1) {
+        if (boxState[i][j] > 1 && isMovable[i][j]) {
           if (j + 1 < 4) {
             if (boxState[i][j + 1] === 1) {
               boxState[i][j + 1] = boxState[i][j];
               boxState[i][j] = 1;
-            } else if (
-              boxState[i][j + 1] === boxState[i][j] &&
-              isMovable[i][j]
-            ) {
+            } else if (boxState[i][j + 1] === boxState[i][j]) {
               isMovable[i][j + 1] = false;
               boxState[i][j + 1] += boxState[i][j];
               boxState[i][j] = 1;
@@ -48,15 +42,12 @@ function getNextBoxState(
   if (direction === 3) {
     for (let i = 0; i < 4; i++) {
       for (let j = 0; j < 4; j++) {
-        if (boxState[i][j] > 1) {
+        if (boxState[i][j] > 1 && isMovable[i][j]) {
           if (i - 1 >= 0) {
             if (boxState[i - 1][j] === 1) {
               boxState[i - 1][j] = boxState[i][j];
               boxState[i][j] = 1;
-            } else if (
-              boxState[i - 1][j] === boxState[i][j] &&
-              isMovable[i][j]
-            ) {
+            } else if (boxState[i - 1][j] === boxState[i][j]) {
               isMovable[i - 1][j] = false;
               boxState[i - 1][j] += boxState[i][j];
               boxState[i][j] = 1;
@@ -69,15 +60,12 @@ function getNextBoxState(
   if (direction === 4) {
     for (let i = 3; i >= 0; i--) {
       for (let j = 0; j < 4; j++) {
-        if (boxState[i][j] > 1) {
+        if (boxState[i][j] > 1 && isMovable[i][j]) {
           if (i + 1 < 4) {
             if (boxState[i + 1][j] === 1) {
               boxState[i + 1][j] = boxState[i][j];
               boxState[i][j] = 1;
-            } else if (
-              boxState[i + 1][j] === boxState[i][j] &&
-              isMovable[i][j]
-            ) {
+            } else if (boxState[i + 1][j] === boxState[i][j]) {
               isMovable[i + 1][j] = false;
               boxState[i + 1][j] += boxState[i][j];
               boxState[i][j] = 1;
